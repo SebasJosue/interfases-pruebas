ac7f9bca68d8d7811ae5d35ddfeec97e6733a825
Sebastián Paredes <146010608+SebasJosue@users.noreply.github.com>
2023-12-21 10:41:29 -0500

    Initial commit

---
+++ ejercicio 1/ejercicio1.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function Student(nombre , correo , promedio){
+    this.firstname
+    this.correo
+    this.promedio= 90;
+}
+
+this.valid= function(Aprobado){
+    this.promedio
+}
+
+this.GeolocationPosition= function () {
+    this.corre.indexof
+}
+
+let student = new Student("Carrillo" , "carrillo@gmail.com", 90);
+
+alert ( Student.validpromedio);
---
+++ ejercicio 2/ejercicio 2.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let ages= ["15", "50", "45"];
+
+for (let i=0; i < ages.length; i++){
+    alert ( `${i+1} ${ages [i]}`);
+}
+
+ages.push("20")
+ages.splice(1,5)
+
